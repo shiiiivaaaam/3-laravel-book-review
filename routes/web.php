d9998b022425cceea720c99de193ab93554806fe
shivam <shivamkumar.official55@gmail.com>
2025-11-01 16:12:35 +0530

    modified routes/web

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('books.index');
 });
 
 Route::resource('books',BookController::class)->only(['index','show']);
